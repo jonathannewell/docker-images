aedac84aad82f14a873fb3ea58c2fe7ca407fa3c
Jonathan Newell <jnewell@pivotal.io>
2018-09-21 12:38:50 -0600

    update to have user adjustable timeout and to su to dse to launch cassandra but stay in the typical user

--- pipeline-cassandra/jon-entrypoint.sh
+++ pipeline-cassandra/jon-entrypoint.sh
@@ -2,15 +2,22 @@
 
 export DS_LICENSE=accept
 
-/entrypoint.sh dse cassandra -f -k -s -g > /var/log/datastax.log &
+#Startup Cassandra
+su dse -p -c 'export PATH=$PATH:$DSE_HOME/bin; /entrypoint.sh dse cassandra -f -k -s -g > /var/log/datastax.log &'
+
+if [ -z "$TIMEOUT" ]; then
+  TIMEOUT=1200
+else
+  TIMEOUT=$((TIMEOUT*2))
+fi
 
 counter=0
 i=1
 sp="/-\|"
 echo -n ' '
 while ! nc -z localhost 9042; do
-  if [ $counter -gt 240 ]; then
-    echo "Cassandra failed to startup within 120 seconds!"
+  if [ $counter -gt $TIMEOUT ]; then
+    printf "\nCassandra failed to startup within [%d] seconds!\n" $((counter/2))
     exit 1
   fi
   sleep 0.5 # wait for 1/10 of the second before check again
@@ -18,6 +25,5 @@ while ! nc -z localhost 9042; do
   printf "\b${sp:i++%${#sp}:1}"
 done
 
-echo "Cassandra is now up..."
-
+printf "\nCassandra is now up...Took: %d seconds!\n" $((counter/2))
 exec "$@"
